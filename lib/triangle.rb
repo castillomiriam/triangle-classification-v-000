144c6f482209597e3c9d4c932ad4c98564b26986
Miriam Castillo <castillo.m.mc@gmail.com>
2018-10-12 02:11:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+
+def initialize(one, two, three)
+  @one = one
+  @two = two
+  @three = three
+end
+
+def kind
+  if (one * two * three) == 0 || (one+two) <= three || (two + three) <= one || (three+one) <= two
+end
+
+  class TriangleError < StandardError
+
+  end
 end
