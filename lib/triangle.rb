528750b0b482ec90faf4768712bff042e0b377cb
Miriam Castillo <castillo.m.mc@gmail.com>
2018-10-12 02:16:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,30 +1,33 @@
 class Triangle
-  attr_accessor :one, :two, :three
-
-def initialize(one, two, three)
-  @one = one
-  @two = two
-  @three = three
-end
-
-def kind
-  if (one * two * three) == 0 || (one+two) <= three || (two + three) <= one || (three+one) <= two
-begin
-  raise TriangleError
-end
-elsif one == two || two == three || one == three
-  :equilateral
-elsif one == two || two == three || one == three
-  :isosceles
-else
-  :scalene
-end
-end
-
-
+  # write code here
+  attr_accessor :s1, :s2, :s3, :t_type
+   def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end    
+   #Is this a valid triangle? 
+  #Raise error if not, (i.e. if any sides are not larger than 0. Or if The sum of the lengths of any two sides is smaller than the 3rd)
+  def kind
+    if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= @s1 || (s3 + s1) <= s2
+      begin 
+        raise TriangleError
+          #puts error.message
+      end   
+    
+    #check for triangle types
+    elsif s1 == s2 && s1 == s3
+      :equilateral
+    elsif s1 == s2 || s2 == s3 || s1 == s3
+       :isosceles
+    else
+       :scalene
+    end
+  end
+ 
   class TriangleError < StandardError
     def message
       puts "Is not a triangle"
-end
-end
+    end
+  end
 end
