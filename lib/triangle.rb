32cbc48539dee05f1bdf25454b2b1c08a4f48b53
Miriam Castillo <castillo.m.mc@gmail.com>
2018-10-12 02:16:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,12 @@
 class Triangle
-  # write code here
-  attr_accessor :s1, :s2, :s3, :t_type
+  
+  attr_accessor :s1, :s2, :s3
    def initialize(s1, s2, s3)
     @s1 = s1
     @s2 = s2
     @s3 = s3
   end
-   #Is this a valid triangle?
-  #Raise error if not, (i.e. if any sides are not larger than 0. Or if The sum of the lengths of any two sides is smaller than the 3rd)
+
   def kind
     if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= @s1 || (s3 + s1) <= s2
       begin
